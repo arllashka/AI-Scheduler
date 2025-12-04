2d8e4af5f2c9dad63db4c2a74fcca86523c04f2e
Arlan Kalin <arlan.kalin@kcell.kz>
2025-12-04 05:39:51 +0500

    Initial Commit

---
+++ AI SchedulerUITests/AI_SchedulerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  AI_SchedulerUITestsLaunchTests.swift
+//  AI SchedulerUITests
+//
+//  Created by Arlan Kalin on 04.12.2025.
+//
+
+import XCTest
+
+final class AI_SchedulerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
